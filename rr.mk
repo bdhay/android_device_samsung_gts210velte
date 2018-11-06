563378af4fc50d322dbd542b1e5ff82045fc301c
bdhay <buddhay@gmx.net>
2018-11-06 12:52:51 +0100

    RRify devtree

---
+++ rr.mk
@@ -0,0 +1,19 @@
+$(call inherit-product, device/samsung/gts210velte/full_gts210velte.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+PRODUCT_NAME := rr_gts210velte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_DEVICE := gts210velte
+PRODUCT_MODEL := SM-T819
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=gts210veltexx \
+    PRIVATE_BUILD_DESC="gts210veltexx-user 7.0 NRD90M T819XXU2BRB2 release-keys"
+
+BUILD_FINGERPRINT := "samsung/gts210veltexx/gts210velte:7.0/NRD90M/T819XXU2BRB2:user/release-keys"
